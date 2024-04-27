a177fe463c55b6aff95f15c28acab24c3ef95cbd
kuisskui <kuikaopong.2002@gmail.com>
2024-04-28 00:28:06 +0700

    log data

--- src/main/java/com/example/Posting/service/UserDetailsServiceImp.java
+++ src/main/java/com/example/Posting/service/UserDetailsServiceImp.java
@@ -2,17 +2,23 @@ package com.example.Posting.service;
 
 import com.example.Posting.entity.User;
 import com.example.Posting.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserDetailsServiceImp implements UserDetailsService {
+
+    Logger logger = LoggerFactory.getLogger(UserDetailsServiceImp.class);
 
     @Autowired
     private UserRepository userRepository;
@@ -28,6 +34,10 @@ public class UserDetailsServiceImp implements UserDetailsService {
 
         List<SimpleGrantedAuthority> authorities = new ArrayList<>();
         authorities.add(new SimpleGrantedAuthority(user.getRole()));
+
+        logger.info(username + " has successfully logged in at "
+                + Instant.now());
+        logger.info(username + " signing in with role " + user.getRole());
 
         return new org.springframework.security.core.userdetails.User(
                 user.getUsername(), user.getPassword(), authorities);
